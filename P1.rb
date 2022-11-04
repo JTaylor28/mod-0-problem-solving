005ad0d437b2d209e4b5348d8dcf36cbe66d2855
James Taylor <jrtaylor0614@gmail.com>
2022-11-04 11:53:58 -0600

    Adds files P1,P2,P3 and completes P1

---
+++ P1.rb
@@ -0,0 +1,14 @@
+
+# 1. Given an array of strings, return only the strings that have exactly 4 characters.
+#  - make array of string 
+#  - use .each and if 
+#  - check elements for character length of 4 
+#     * .length for method
+#  - print names |letter| 
+
+letters = ["aa","bbbb","cccccccc","ddddddd","eeeeeee","ffff","ggggggg"]
+letters.each do |letter|
+    if letter.length == 4
+        p letter
+    end
+end
